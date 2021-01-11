1d872b78a43107507e8908f1e4b43882ac0012dc
Yizhao He <heyizhaobupt@gmail.com>
2021-01-11 00:01:33 -0800

    Add demo for nativescript-generate-pdf

--- apps/demo/src/plugin-demos/nativescript-generate-pdf.ts
+++ apps/demo/src/plugin-demos/nativescript-generate-pdf.ts
@@ -1,10 +1,42 @@
-import { Observable, EventData, Page } from '@nativescript/core';
-import { DemoSharedNativescriptGeneratePdf } from '@demo/shared';
-import {} from '@knotes/nativescript-generate-pdf';
+import { GeneratePdf } from '@knotes/nativescript-generate-pdf';
+import { WebView, LoadEventData } from '@nativescript/core/ui/web-view';
+import { NavigatedData, Page } from '@nativescript/core/ui/page';
+import { Observable, knownFolders, isIOS } from '@nativescript/core';
+import * as permissions from 'nativescript-permissions';
+import { html } from './html';
 
-export function navigatingTo(args: EventData) {
+let webView: any;
+
+export function onNavigatingTo(args: NavigatedData) {
 	const page = <Page>args.object;
-	page.bindingContext = new DemoModel();
+
+	page.bindingContext = new DemoGeneratePdfModel();
 }
 
-export class DemoModel extends DemoSharedNativescriptGeneratePdf {}
+export function onWebViewLoaded(args: LoadEventData) {
+	webView = (<WebView>args.object).nativeView;
+	new GeneratePdf().createPdf(webView, 'MyPdfFileName');
+}
+
+export function onRefresh(args: LoadEventData) {
+	webView.reload();
+}
+
+export class DemoGeneratePdfModel extends Observable {
+	public webViewSRC: string;
+
+	constructor() {
+		super();
+		this.webViewSRC = this.getHtmlPath();
+		console.log('webViewSRC', this.webViewSRC);
+	}
+
+	getHtmlPath() {
+		if (!isIOS) {
+			permissions.requestPermission([android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE], 'Need to save files to disk');
+		}
+		const htmlFile = knownFolders.temp().getFile('index.html');
+		htmlFile.writeTextSync(html);
+		return htmlFile.path;
+	}
+}
